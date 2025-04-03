426d4353c8e765f05b30b7cdf1bb13cee08ffe0d
Dossy Shiobara <dossy@panoptic.com>
2025-04-03 12:57:47 -0400

    chore: add new prop from upstream

--- src/components/PluginSettings/components/SettingTextComponent.tsx
+++ src/components/PluginSettings/components/SettingTextComponent.tsx
@@ -90,6 +90,7 @@ export function SettingTextComponent({ option, pluginSettings, definedSettings, 
                     onChange={handleChange}
                     placeholder={option.placeholder ?? "Enter a value"}
                     disabled={option.disabled?.call(definedSettings) ?? false}
+                    maxLength={null}
                     {...option.componentProps}
                 />
             }
