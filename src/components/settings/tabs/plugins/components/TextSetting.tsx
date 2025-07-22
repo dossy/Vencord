36d8ac3d34a892e4a0ae1d0f296803aa93b658da
Dossy Shiobara <dossy@panoptic.com>
2025-07-22 13:30:53 -0400

    feat: multiline textarea settings input

--- src/components/settings/tabs/plugins/components/TextSetting.tsx
+++ src/components/settings/tabs/plugins/components/TextSetting.tsx
@@ -17,7 +17,7 @@
 */
 
 import { PluginOptionString } from "@utils/types";
-import { React, TextInput, useState } from "@webpack/common";
+import { React, TextArea, TextInput, useState } from "@webpack/common";
 
 import { resolveError, SettingProps, SettingsSection } from "./Common";
 
@@ -38,15 +38,52 @@ export function TextSetting({ option, pluginSettings, definedSettings, id, onCha
 
     return (
         <SettingsSection name={id} description={option.description} error={error}>
-            <TextInput
-                type="text"
-                placeholder={option.placeholder ?? "Enter a value"}
-                value={state}
-                onChange={handleChange}
-                maxLength={null}
-                disabled={option.disabled?.call(definedSettings) ?? false}
-                {...option.componentProps}
-            />
+            {!option.componentProps?.multiline
+                ? <TextInput
+                    type="text"
+                    placeholder={option.placeholder ?? "Enter a value"}
+                    value={state}
+                    onChange={handleChange}
+                    maxLength={null}
+                    disabled={option.disabled?.call(definedSettings) ?? false}
+                    {...option.componentProps}
+                />
+                : <TextArea
+                    style={{
+                        // textArea-2CLwUE:
+                        // background-color: "transparent",
+                        // resize: "none",
+                        border: "none",
+                        // @ts-expect-error
+                        "-webkit-appearance": "none",
+                        "-moz-appearance": "none",
+                        appearance: "none",
+                        "-webkit-box-sizing": "border-box",
+                        "box-sizing": "border-box",
+                        "font-weight": 400,
+                        "font-size": "1rem",
+                        "line-height": "1.375rem",
+                        width: "100%",
+                        // height: "44px",
+                        "min-height": "44px",
+                        color: "var(--text-normal)",
+                        // "padding-left": 0,
+                        // "padding-right": "10px",
+                        // fontSize16Padding-XoMpjI:
+                        "padding-bottom": "11px",
+                        "padding-top": "11px",
+                        // textAreaWithoutAttachmentButton-1as0NS:
+                        "padding-left": "16px",
+                        // additional styles:
+                        background: "var(--input-background)",
+                        resize: "vertical"
+                    }}
+                    value={state}
+                    onChange={handleChange}
+                    placeholder={option.placeholder ?? "Enter a value"}
+                    disabled={option.disabled?.call(definedSettings) ?? false}
+                    {...option.componentProps}
+                />}
         </SettingsSection>
     );
 }
